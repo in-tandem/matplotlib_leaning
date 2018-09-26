cab5fd4c5f1c30474f9ccc9ff68ca48337bcaa6e
somak <somaktukai@gmail.com>
2018-09-26 18:08:29 +0530

    first trial

---
+++ simple_graph.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plot
+
+print('i am going to draw a simple graph')
+
+x_axis = [10, 20, 30, 40, 66, 89]
+y_axis = [2.2, 1.1, 0, 3, -9, 99]
+
+plot.plot(x_axis,y_axis)
+
+plot.show()
