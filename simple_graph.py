cb369b9f116d55f65a8e07b2cc5754de273f6ed4
somak <somaktukai@gmail.com>
2018-09-26 18:18:51 +0530

    added labels

--- simple_graph.py
+++ simple_graph.py
@@ -1,3 +1,7 @@
+
+## i am going to plot using simple lists of data
+## i am going to label x and y axis
+
 import matplotlib.pyplot as plot
 
 print('i am going to draw a simple graph')
@@ -6,5 +10,6 @@ x_axis = [10, 20, 30, 40, 66, 89]
 y_axis = [2.2, 1.1, 0, 3, -9, 99]
 
 plot.plot(x_axis,y_axis)
-
+plot.xlabel("Number of Routers")
+plot.ylabel("Distances from Master Router( -ve means inner circle) ")
 plot.show()
