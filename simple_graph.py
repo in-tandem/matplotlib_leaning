9a06f3dd89ffc28ff063270632d2e54bd5bfb32f
somak <somaktukai@gmail.com>
2018-09-26 18:25:29 +0530

    added title

--- simple_graph.py
+++ simple_graph.py
@@ -1,7 +1,8 @@
 
 ## i am going to plot using simple lists of data
 ## i am going to label x and y axis
-
+## i am going to add color
+## i am adding title to the graph
 import matplotlib.pyplot as plot
 
 print('i am going to draw a simple graph')
@@ -9,7 +10,8 @@ print('i am going to draw a simple graph')
 x_axis = [10, 20, 30, 40, 66, 89]
 y_axis = [2.2, 1.1, 0, 3, -9, 99]
 
-plot.plot(x_axis,y_axis)
+plot.plot(x_axis,y_axis, color="blue")
 plot.xlabel("Number of Routers")
 plot.ylabel("Distances from Master Router( -ve means inner circle) ")
+plot.title("some graph silly silly")
 plot.show()
