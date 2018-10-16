664afba5d27993af87ae9a966d128222983aee8c
somak <somaktukai@gmail.com>
2018-10-16 17:31:13 +0530

    histogram of migration data..firt draft

--- panda_plot_integration.py
+++ panda_plot_integration.py
@@ -16,10 +16,10 @@ immigration_to_canada_data.columns = list(map(str, immigration_to_canada_data.co
 # convert all columns names to lowercase 
 immigration_to_canada_data.rename(str.lower, inplace=True, axis='columns')
 
-#rename a few columns for our convinience
+# rename a few columns for our convinience
 immigration_to_canada_data.rename(columns = {'odname' : 'country', 'areaname':'continent'}, inplace=True)
 
-#add an additional column called total 
+# add an additional column called total 
 immigration_to_canada_data['total'] = immigration_to_canada_data.sum(axis=1, numeric_only= True)
 
 def draw_united_nations_migration_line_plot_for_country(country):
@@ -74,5 +74,37 @@ def draw_sample_histogram():
     plot.title("Random sampliing of ten thousand numbres")
     plot.show()
 
-draw_immigration_country_wise_area_chart()
+def draw_histogram_for_migration_in_particular_year(year):
+    immigration_to_canada_data.reset_index()
+    immigration_to_canada_data.sort_values(['total'], ascending = False, inplace = True)
+
+    years = list(map(str,range(1980,2014)))
+    columns = ['country'] + years
+
+    ## use only the columns you require, and take the topmost 5 countries
+    data_frame = immigration_to_canada_data[columns]
+    
+    ## index which is set in pandas is used to plot in x axis. we would hence transpose
+    data_frame.set_index('country', inplace = True)
+    
+    data_frame = data_frame.transpose()
+
+    ## my columns are going to become rows. now remember i converted my columns to strings
+    ## unless i change them back to numeric, x values will come as empty
+    data_frame.index = data_frame.index.map(int) 
+
+    print(data_frame.loc[year])
+
+    data_frame.loc[year].plot(kind='hist', color='purple')
+
+    plot.title("HIstogram showing migration across globe for the year %d" %year)
+    plot.xlabel("Number of Migrants")
+    plot.ylabel("Number of Countries")
+    plot.show()
+
+
+draw_histogram_for_migration_in_particular_year(2013)
+
+
+# draw_immigration_country_wise_area_chart()
 # draw_united_nations_migration_line_plot_for_country('Haiti')
