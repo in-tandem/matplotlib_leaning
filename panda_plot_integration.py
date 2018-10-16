70bc76a0973c733ccd5497f98a81df15ac278e67
somak <somaktukai@gmail.com>
2018-10-16 12:27:41 +0530

    area chart for immigration country wise and year wise

--- panda_plot_integration.py
+++ panda_plot_integration.py
@@ -10,13 +10,22 @@ immigration_to_canada_data = panda.read_excel(united_nations_immigration_data_re
                             skipfooter=2
                              )
 
+# convert all column names to string values
 immigration_to_canada_data.columns = list(map(str, immigration_to_canada_data.columns))
-# print(immigration_to_canada_data.columns.tolist())
+
+# convert all columns names to lowercase 
+immigration_to_canada_data.rename(str.lower, inplace=True, axis='columns')
+
+#rename a few columns for our convinience
+immigration_to_canada_data.rename(columns = {'odname' : 'country', 'areaname':'continent'}, inplace=True)
+
+#add an additional column called total 
+immigration_to_canada_data['total'] = immigration_to_canada_data.sum(axis=1, numeric_only= True)
 
 def draw_united_nations_migration_line_plot_for_country(country):
-    
-    immigration_to_canada_data.set_index('OdName', inplace=True)
-    print(immigration_to_canada_data.head(2))
+     
+    immigration_to_canada_data.set_index('country', inplace=True)
+    # print(immigration_to_canada_data.head(2))
     # print(immigration_to_canada_data.loc['Haiti'])
     
     years = list( map(str,range(1980, 2013)))
@@ -30,6 +39,33 @@ def draw_united_nations_migration_line_plot_for_country(country):
     plot.show()
     
 
+def draw_immigration_country_wise_area_chart():
+
+    immigration_to_canada_data.reset_index()
+    immigration_to_canada_data.sort_values(['total'], ascending = False, inplace = True)
+
+    years = list(map(str,range(1980,2014)))
+    columns = ['country'] + years
+
+    ## use only the columns you require, and take the topmost 5 countries
+    data_frame = immigration_to_canada_data[columns].head()
+    
+    ## index which is set in pandas is used to plot in x axis. we would hence transpose
+    data_frame.set_index('country', inplace = True)
+    
+    data_frame = data_frame.transpose()
+
+    ## my columns are going to become rows. now remember i converted my columns to strings
+    ## unless i change them back to numeric, x values will come as empty
+    data_frame.index = data_frame.index.map(int) 
+
+    data_frame.plot(kind= 'area')
+    plot.title("Area Chart of Top Five Migrating nations")
+    plot.xlabel("Years")
+    plot.ylabel("Number of Migrants")
+    plot.show()
+
+
 def draw_sample_histogram():
     
     random_ten_thousand_numbers = random.sample(range(600000), 10000)
@@ -38,5 +74,5 @@ def draw_sample_histogram():
     plot.title("Random sampliing of ten thousand numbres")
     plot.show()
 
-
-draw_united_nations_migration_line_plot_for_country('Haiti')
+draw_immigration_country_wise_area_chart()
+# draw_united_nations_migration_line_plot_for_country('Haiti')
